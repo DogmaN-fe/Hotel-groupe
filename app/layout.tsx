c21cae847757640663764e2b8de13ff6428d0472
Eduard <126475818+DogmaN-fe@users.noreply.github.com>
2024-06-14 14:16:40 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={`${styles.body} ${roboto.className}`}>
         <header className={styles.header}>
           <HeaderLogo />
